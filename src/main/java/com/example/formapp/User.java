043d44033847913ef78f7ba7a4a2ed96a7cf5ac7
VAMSHI KRISHNA K <147719676+vamshik2506@users.noreply.github.com>
2025-05-14 10:05:14 +0530

    Update User.java

--- src/main/java/com/example/formapp/User.java
+++ src/main/java/com/example/formapp/User.java
@@ -3,6 +3,19 @@ package com.example.formapp;
 public class User {
     private String name;
     private String email;
-    
-    // Getters and Setters
+
+    // Getters & Setters
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
