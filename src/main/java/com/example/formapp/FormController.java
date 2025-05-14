8bc077819fd0246d6ac254570335ec6c6bbc5d92
VAMSHI KRISHNA K <147719676+vamshik2506@users.noreply.github.com>
2025-05-14 10:04:44 +0530

    Update FormController.java

--- src/main/java/com/example/formapp/FormController.java
+++ src/main/java/com/example/formapp/FormController.java
@@ -1,5 +1,23 @@
 package com.example.formapp;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
 public class FormController {
-    // To be implemented
+
+    @GetMapping("/")
+    public String showForm(Model model) {
+        model.addAttribute("user", new User());
+        return "form";
+    }
+
+    @PostMapping("/submit")
+    public String submitForm(@ModelAttribute User user, Model model) {
+        model.addAttribute("name", user.getName());
+        model.addAttribute("email", user.getEmail());
+        System.out.println("Received: " + user.getName() + ", " + user.getEmail());
+        return "form";
+    }
 }
